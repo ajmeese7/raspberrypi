acbfea9a936e92829cd73a40a315e76259d86f69
Aaron Meese <ajmeese7@gmail.com>
2021-11-21 16:16:59 -0500

    Removed a lot of output
    There are now two annoying things that still show, which is `Skipping already downloaded file` and `E: Could not get lock`. Something will have to be done about these eventually, but for now the script works.

--- nginx.sh
+++ nginx.sh
@@ -2,35 +2,43 @@
 # Much of his file is modified from this amazing article:
 # https://www.linuxbabe.com/security/modsecurity-nginx-debian-ubuntu
 
+### Variables that will be used throughout the program
+nginx_mainline_source=/etc/apt/sources.list.d/nginx-mainline.list
+modsec_dir=/usr/local/src/ModSecurity/
+modsec_nginx_dir=/usr/local/src/ModSecurity-nginx
+nginx_conf=/etc/nginx/nginx.conf
+modsec_conf=/etc/nginx/modsec/modsecurity.conf
+modsec_main_conf=/etc/nginx/modsec/main.conf
+nginx_default_site=/etc/nginx/sites-available/default
+
 ##
 # Install the lastest version of nginx
 ##
-curl -sSL https://packages.sury.org/nginx-mainline/README.txt | bash -x
-apt update && apt upgrade -y
-apt install nginx-core nginx-common nginx nginx-full -y
+
+wget -q -O /etc/apt/trusted.gpg.d/nginx-mainline.gpg https://packages.sury.org/nginx-mainline/apt.gpg
+sh -c 'echo "deb https://packages.sury.org/nginx-mainline/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/nginx-mainline.list' > /dev/null
+
+apt-get update -y &> /dev/null
+apt-get upgrade -y &> /dev/null
+apt-get -y install apt-transport-https lsb-release ca-certificates curl \
+	nginx-core nginx-common nginx nginx-full &> /dev/null
+echo "Installed the latest version of nginx..."
 
 ##
 # Enable the source code repository in order to download nginx source code
 ##
 
-nginx_mainline_source=/etc/apt/sources.list.d/nginx-mainline.list
+nginx_binary_link=`cat $nginx_mainline_source`
+nginx_source_code_link="deb-src $(echo "$nginx_binary_link" | cut -d' ' -f 2-)"
+        # takes a substring of $nginx_binary_link
+       	# `-d' '` = splits the string at the space (' ') delimeter
+        # `-f 2-` = grabs the second portion of the split through the end, because `-f` uses a 1-based index
+       	# prepends 'deb-src ' to the output, which is the mainline link for your operating system version
+        # this isn't hardcoded because some devices may run on buster, others on bullseye, etc.
 
-# makes sure the deb-src repo isn't already in your mainline source file
-if ! grep -q -c deb-src $nginx_mainline_source;
-then
-	nginx_binary_link=`cat $nginx_mainline_source`
-	nginx_source_code_link="deb-src $(echo "$nginx_binary_link" | cut -d' ' -f 2-)"
-	        # takes a substring of $nginx_binary_link
-        	# `-d' '` = splits the string at the space (' ') delimeter
-	        # `-f 2-` = grabs the second portion of the split through the end, because `-f` uses a 1-based index
-        	# prepends 'deb-src ' to the output, which is the mainline link for your operating system version
-	        # this isn't hardcoded because some devices may run on buster, others on bullseye, etc.
-	echo $nginx_source_code_link >> $nginx_mainline_source
-else
-	echo "The 'deb-src' link is already in your mainline source file! Skipping step..."
-fi
-
-apt update
+echo $nginx_source_code_link >> $nginx_mainline_source
+echo "Uncommented the 'deb-src' line in your nginx-mainline.list file..."
+apt-get update -y &> /dev/null
 
 ##
 # Download nginx source package
@@ -41,39 +49,35 @@ username="${SUDO_USER:-${USER}}"
 chown $username:$username /usr/local/src/ -R
 mkdir -p /usr/local/src/nginx
 cd /usr/local/src/nginx/
-apt install dpkg-dev
 
-# command gives an error that can safely be ignored, so it is redirected to /dev/null
-apt source nginx 2> /dev/null
+apt-get -y install dpkg-dev > /dev/null
+apt-get source nginx &> /dev/null
 
 ##
 # Install libmodsecurity3
 ##
 
-modsec_dir=/usr/local/src/ModSecurity/
-apt install gcc make build-essential autoconf automake libtool \
+apt-get -qq install gcc make build-essential autoconf automake libtool \
         libcurl4-openssl-dev liblua5.3-dev libfuzzy-dev ssdeep \
         gettext pkg-config libpcre3 libpcre3-dev libxml2 libxml2-dev \
-        libcurl4 libgeoip-dev libyajl-dev doxygen libmaxminddb-dev git -y
+        libcurl4 libgeoip-dev libyajl-dev doxygen libmaxminddb-dev git > /dev/null
 
-# if the directory already exists, this steps have likely already been taken
 if [ ! -d "$modsec_dir" ];
 then
 	git clone --depth 1 -b v3/master --single-branch https://github.com/SpiderLabs/ModSecurity $modsec_dir
 	cd $modsec_dir
-	git submodule init
-	git submodule update
-	./build.sh
-
-	# command gives an error that can safely be ignored, so it is redirected to /dev/null
-	./configure 2> /dev/null
+	git submodule init > /dev/null
+	git submodule update > /dev/null
+	./build.sh > /dev/null
+	./configure > /dev/null
 
 	# the number must be <= the number of cores your machine possesses,
 	# so my command defaults to a maximum of 1 less than the total number of cores;
 	# this is unless your machine only has 1 core, in which case it will use that core
 	max_num_cores=`getconf _NPROCESSORS_ONLN`-1
-	make -j$(( $max_num_cores < 1 ? 1 : $max_num_cores ))
-	make install
+	make -j$(( $max_num_cores < 1 ? 1 : $max_num_cores )) > /dev/null
+	make install > /dev/null
+	echo "Cloned and configured ModSecurity from GitHub..."
 else
 	echo "You already have ModSecurity installed! Skipping step..."
 fi
@@ -82,16 +86,16 @@ fi
 # Download and Compile ModSecurity v3 Nginx Connector Source Code
 ##
 
-modsec_nginx_dir=/usr/local/src/ModSecurity-nginx
 if [ ! -d "$modsec_nginx_dir" ];
 then
-	git clone --depth 1 https://github.com/SpiderLabs/ModSecurity-nginx.git $modsec_nginx_dir
+	git clone --depth 1 https://github.com/SpiderLabs/ModSecurity-nginx.git $modsec_nginx_dir > /dev/null
 	cd /usr/local/src/nginx/nginx-*
-	apt build-dep nginx
-	apt install uuid-dev -y
-	./configure --with-compat --add-dynamic-module=$modsec_nginx_dir
-	make modules
+	apt-get build-dep nginx &> /dev/null
+	apt-get -qq install uuid-dev > /dev/null
+	./configure --with-compat --add-dynamic-module=$modsec_nginx_dir > /dev/null
+	make modules > /dev/null
 	cp objs/ngx_http_modsecurity_module.so /usr/share/nginx/modules/
+	echo "Cloned and configured ModSecurity-nginx from GitHub..."
 else
 	echo "You already have ModSecurity-nginx installed! Skipping step..."
 fi
@@ -100,72 +104,106 @@ fi
 # Load the ModSecurity v3 Nginx Connector Module
 ##
 
-nginx_conf=/etc/nginx/nginx.conf
 if ! grep -q -c ngx_http_modsecurity_module $nginx_conf;
 then
 	# https://www.baeldung.com/linux/insert-line-specific-line-number#using-sed
 	sed -i '5 i load_module modules/ngx_http_modsecurity_module.so;' $nginx_conf
 
-	# https://stackoverflow.com/a/20026432/6456163;
-	# searches for `http {` in the file and adds 1 to the line number,
-	# so we can insert our custom configuration on the next line
-	http_line_number=`awk '/http {/{ print NR; exit }' $nginx_conf`
-	sed -i "$line_number_to_insert_config i \\\\tmodsecurity on;" $nginx_conf
-	sed -i "$(( $line_number_to_insert_config + 1 )) i \\\\tmodsecurity_rules_file /etc/nginx/modsec/main.conf;" $nginx_conf
+	sed -i '/http {/a \
+        modsecurity on;\
+	modsecurity_rules_file /etc/nginx/modsec/main.conf;' $nginx_conf
+	echo "nginx.conf has been configured to include ModSecurity..."
 else
 	echo "You have already configured nginx to include ModSecurity! Skipping step..."
 fi
 
 # ModSecurity configuration file manipulation
 if [ ! -d /etc/nginx/modsec ]; then mkdir /etc/nginx/modsec; fi
-modsec_conf=/etc/nginx/modsec/modsecurity.conf
 cp /usr/local/src/ModSecurity/modsecurity.conf-recommended $modsec_conf
 sed -i 's/SecRuleEngine DetectionOnly/SecRuleEngine On/' $modsec_conf
 sed -i 's/SecAuditLogParts ABIJDEFHZ/SecAuditLogParts ABCEFHJKZ/' $modsec_conf
 sed -i 's/SecResponseBodyAccess On/SecResponseBodyAccess Off/' $modsec_conf
 
 # create and populate the main.conf file
-modsec_main_conf=/etc/nginx/modsec/main.conf
 if [ ! -f $modsec_main_conf ];
 then
 	echo Include /etc/nginx/modsec/modsecurity.conf >> $modsec_main_conf
+	echo "Created and configured the /etc/nginx/modsec/main.conf file..."
 fi
 
 # copy the Unicode mapping file
 cp /usr/local/src/ModSecurity/unicode.mapping /etc/nginx/modsec/
 
-systemctl restart nginx
-
 ##
 # Enable OWASP Core Rule Set
 ##
 
-# https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8#gistcomment-2736901
 if [ ! -d /tmp/coreruleset ]; then mkdir /tmp/coreruleset; fi
 cd /tmp/coreruleset
+
+# https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8#gistcomment-2736901
 curl --silent "https://api.github.com/repos/coreruleset/coreruleset/releases/latest" |
     grep '"tag_name":' |
-    sed -E 's/.*"([^"]+)".*/\1/' |
-    xargs -I {} curl -sOL "https://github.com/coreruleset/coreruleset/archive/"{}'.tar.gz'
-tar xvf *.tar.gz && rm *.tar.gz
+    sed -r 's/.*"([^"]+)".*/\1/' |
+    xargs -I {} curl --silent -sOL "https://github.com/coreruleset/coreruleset/archive/"{}'.tar.gz'
+tar xf *.tar.gz && rm *.tar.gz
 
 coreruleset=`ls`
-mv $coreruleset /etc/nginx/modsec
-if [ -f /etc/nginx/modsec/$coreruleset/crs-setup.conf.example ];
+if [ ! -d  /etc/nginx/modsec/$coreruleset ];
 then
+	mv $coreruleset /etc/nginx/modsec
 	mv /etc/nginx/modsec/$coreruleset/crs-setup.conf.example /etc/nginx/modsec/$coreruleset/crs-setup.conf
+	echo "Moved your coreruleset directory to /etc/nginx/modsec..."
 fi
 
 if ! grep -q -c crs-setup.conf $modsec_main_conf;
 then
 	echo Include /etc/nginx/modsec/$coreruleset/crs-setup.conf >> $modsec_main_conf
 	echo "Include /etc/nginx/modsec/$coreruleset/rules/*.conf" >> $modsec_main_conf
+	echo "Your crs-setup.conf now includes all the rules from ModSecurity..."
+else
+	echo "Your crs-setup.conf already includes ModSecurity! Skipping step..."
 fi
 
 rm -rf /tmp/coreruleset
-systemctl restart nginx
+cd ~
+
+##
+# Extra security measures
+##
+
+if grep -q -c "# server_tokens" $nginx_conf;
+then
+	# disable leakage of information about nginx version
+	sed -i 's/# server_tokens/server_tokens/' $nginx_conf
+	sed -i '/server_tokens/a \
+        \
+        \# enable XSS-S protection\
+        add_header X-XSS-Protection \"1; mode=block\";\
+        \
+        \# prevent clickjacking attacks\
+        add_header X-Frame-Options \"SAMEORIGIN\";' $nginx_conf
+	echo "Added additional security measures to your nginx.conf file..."
+else
+	echo "Already configured additional nginx security measures! Skipping step..."
+fi
+
+if ! grep -q -c "return 405" $nginx_default_site;
+then
+	sed -i '/listen \[::\]:80 default_server/a \
+	\
+	\# disable undesirable HTTP methods, ex. DELETE, TRACE \
+	if ($request_method \!~ ^(GET|HEAD|POST)$ ) {\
+		return 405;\
+	}' $nginx_default_site
+	echo "Blocked undesirable HTTP methods in your /etc/nginx/sites-available/default file..."
+else
+	echo "Already blocking undesirable HTTP methods! Skipping step..."
+fi
+
+systemctl reload nginx > /dev/null
 systemctl enable nginx
 
-printf "##############################\n"
-printf "# FINISHED CONFIGURING NGINX #\n"
-printf "##############################\n"
+printf "\n##############################\n"
+printf   "# FINISHED CONFIGURING NGINX #\n"
+printf   "##############################\n"
