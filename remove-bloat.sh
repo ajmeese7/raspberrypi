f9f603522d8f25a2a40aeb0eb9dc50d44233ae08
Aaron Meese <ajmeese7@gmail.com>
2021-11-26 18:02:02 -0500

    Calculate space reclaimed
    The `remove-bloat.sh` file now gives users a numerical value of exactly how much space was taken back when the specified bloatware was removed from the system.

--- remove-bloat.sh
+++ remove-bloat.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# used to calculate the space reclaimed at the end of the script
+storage_before=`df -m --output=avail --total | awk 'END {print $1}'`
+
 # remove wolfram
 apt purge wolfram-engine -y
 
@@ -8,3 +11,8 @@ apt remove --purge libreoffice* -y
 
 # remove all helper packages
 apt clean && apt autoremove -y
+
+storage_after=`df -m --output=avail --total | awk 'END {print $1}'`
+storage_difference=$(( storage_after - storage_before ))
+
+echo "remove-bloat.sh freed up $storage_difference MB on your system!"
