b9a6316eba66432692aa9919b7361b0045554e25
Aaron Meese <ajmeese7@gmail.com>
2021-11-26 17:57:08 -0500

    nslookup command
    This tool is helpful for if you are ever trying to debug the DNS associated with your web server.

--- helpful.sh
+++ helpful.sh
@@ -6,3 +6,6 @@ apt-get install software-properties-common -y
 # enables the use of the `apt-build-dep` command
 sed 's/#deb-src/deb-src/' /etc/apt/sources.list
 apt-get update -y
+
+# enables the use of the nslookup tool
+apt-get install bind9-utils dnsutils -y
