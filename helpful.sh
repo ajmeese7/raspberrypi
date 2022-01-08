4ec5171bfac0b387b86187f28de0015bf3b50413
Aaron Meese <ajmeese7@gmail.com>
2022-01-08 10:45:22 -0500

    GH CLI

--- helpful.sh
+++ helpful.sh
@@ -9,3 +9,9 @@ apt-get update -y
 
 # enables the use of the nslookup tool
 apt-get install bind9-utils dnsutils -y
+
+# install the GitHub CLI
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
