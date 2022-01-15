6424d5250006dad4ebd12be3a19ada24b4ebb1a2
Aaron Meese <ajmeese7@gmail.com>
2022-01-15 14:36:58 -0500

    Install PHP

--- helpful.sh
+++ helpful.sh
@@ -13,5 +13,11 @@ apt-get install bind9-utils dnsutils -y
 # install the GitHub CLI
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt install gh
+sudo apt update -y
+sudo apt install gh -y
+
+# install PHP
+wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
+sudo echo "deb https://packages.sury.org/php/ buster main" | sudo tee /etc/apt/sources.list.d/php.list
+sudo apt update -y
+sudo apt install php php-fpm -y
